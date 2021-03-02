241d6ea54fefa9a34b57b6a5ae62e0c4dce7b0ad
Jan Romanowski <jan.romanowski@fujitsu.com>
2021-03-02 15:47:06 +0100

    Update main.go
    Added basic mux routing

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"time"
+	"io"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, world!\n")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "This application is very cool.\n")
+}
+
+// Route declaration
+func router() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+	r.HandleFunc("/about", about)
+	return r
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	router := router()
+	srv := &http.Server{
+		Handler: router,
+		Addr:    "127.0.0.1:9100",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
